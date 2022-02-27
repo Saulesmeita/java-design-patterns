d7bad764182698f7359b00d03c6f582e5f538c17
Anna Terehova <anna.terehova@gmail.com>
2022-02-27 16:25:02 +0200

    Add Transposition

---
+++ matrix/src/main/java/org/saulesmeita/matrix/Transposition.java
@@ -0,0 +1,22 @@
+package org.saulesmeita.matrix;
+
+import java.util.function.IntFunction;
+import java.util.function.IntToDoubleFunction;
+import java.util.function.IntUnaryOperator;
+
+public enum Transposition {
+    MAIN_DIAGONAL(size -> i -> i / size + i % size * size),
+    SIDE_DIAGONAL(size -> i -> size * (size - i % size) - i / size - 1),
+    VERTICAL_AXIS(size -> i -> size - i % size - 1 + i / size * size),
+    HORIZONTAL_AXIS(size -> i -> size * (size - i / size - 1) + i % size);
+
+    private final IntFunction<IntUnaryOperator> function;
+
+    Transposition(final IntFunction<IntUnaryOperator> function) {
+        this.function = function;
+    }
+
+    IntToDoubleFunction getFormula(final Matrix matrix) {
+        return i -> matrix.element(function.apply(matrix.rows()).applyAsInt(i));
+    }
+}
